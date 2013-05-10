0c2203cc69394ef6145f61a9272e066f12ddb6c2
Praaveen Ranganathan <praaveen@eggheadcreative.com>
2013-05-10 17:04:42 +0530

    Getting started on devkit module level methods

--- lib/devkit.rb
+++ lib/devkit.rb
@@ -3,5 +3,28 @@ $:.unshift(File.join(File.dirname(__FILE__), 'devkit'))
 require "version"
 
 module Devkit
-  # Your code goes here...
+  def self.init!
+    puts "init"
+  end
+
+  def self.list
+    puts "list"
+  end
+
+  def self.clean!
+    puts "clean"
+  end
+
+  def self.remove!(developer_nick_name)
+    puts developer_nick_name
+  end
+
+  def self.switch!(developer_nick_name)
+    puts developer_nick_name
+  end
+
+  def self.add!
+    puts "add"
+  end
 end
+
