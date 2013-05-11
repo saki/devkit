029a615c148743b38bbb3a4fe20c42751555bb58
Praaveen Ranganathan <praaveen@eggheadcreative.com>
2013-05-11 11:50:16 +0530

    Minor formatting

--- lib/devkit.rb
+++ lib/devkit.rb
@@ -6,23 +6,22 @@ require 'yaml'
 
 module Devkit
   DEVELOPERS_FILE_PATH = File.expand_path('~/.developers')
-
   # Check if .developers file exists in user directory
   # Else create an empty .developers file
   # Prompt for adding new developers
   def self.init!
     if check_if_developers_file_exists?
       puts "developer file exists."
-      #Prompt user that the file already exists and if the user really want to start over ask him to user devkit --clean
+    #Prompt user that the file already exists and if the user really want to start over ask him to user devkit --clean
     else
       puts "developer file does not exist."
-      #Create file
-      #Promt user to add more developers      
+    #Create file
+    #Prompt user to add more developers
     end
   end
 
-  def self.list 
-    puts developers.to_yaml  
+  def self.list
+    puts developers.to_yaml
   end
 
   def self.clean!
@@ -33,12 +32,11 @@ module Devkit
   end
 
   def self.remove!(developer_nick_name)
-    developer = developers[developer_nick_name]
-    puts developer
-    
     #Prompt the user to make sure he actually want to delete developer
     #if yes delete developer from .developers
     #if no exit
+    developer = developers[developer_nick_name]
+    puts developer
   end
 
   def self.switch!(developer_nick_name)
@@ -53,11 +51,11 @@ module Devkit
   protected
 
   def self.developers
-    if check_if_developers_file_exists? 
+    if check_if_developers_file_exists?
       YAML.load_file(DEVELOPERS_FILE_PATH)
     else
-      {}         
-    end 
+      {}
+    end
   end
 
   def self.check_if_developers_file_exists?
