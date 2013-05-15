b3b939cba91758ba09b7e4f6ac005b02c49fbbf6
Tripura Konda <tripura@eggheadcreative.com>
2013-05-16 00:56:03 +0530

    Switching git config, identities

--- lib/devkit.rb
+++ lib/devkit.rb
@@ -6,6 +6,7 @@ require "yaml"
 
 module Devkit
   DEVELOPERS_FILE_PATH = File.expand_path('~/.developers')
+
   def self.init!
     if check_if_developers_file_exists?
       if agree(".developers file already exist.You want to over ride existing file? (y/n)", true)
@@ -36,20 +37,21 @@ module Devkit
     existing_developers = developers
     developer = existing_developers[developer_nick_name]
     if developer && agree("Are you sure you want to delete #{developer['Full Name']} (y/n)?", true)
-      new_developers = existing_developers.reject {|dev| dev == developer_nick_name}
+      new_developers = existing_developers.reject { |dev| dev == developer_nick_name }
       self.clear_existing_developers_file
       File.open(DEVELOPERS_FILE_PATH, 'a+') do |f|
         f.write(new_developers.to_yaml)
       end
     else
-      puts "Developer does not exist."
-      self.list
+      puts "#{developer_nick_name} does not exist in .developers list"
+      #self.list
     end
   end
 
   def self.switch!(developer_nick_name)
     developer = developers[developer_nick_name]
-    puts developer
+    switch_git_config(developer)
+    switch_identity(developer)
   end
 
   def self.add!
@@ -92,6 +94,57 @@ module Devkit
       return true
     else
       return false
+    end
+  end
+
+  def self.switch_git_config(developer)
+    print "Switching git config to #{developer['Full Name']}, "
+    $stdout.flush
+    system("git config --global user.name '#{developer['Full Name']}'")
+    system("git config --global user.email #{developer['Email']}")
+    system("git config --global github.user #{developer['Github Id']}")
+
+    check_git_config(developer)
+  end
+
+  def self.check_git_config(developer)
+    check = developer["Full Name"] == %x[git config --global user.name].chomp &&
+        developer["Email"] == %x[git config --global user.email].chomp &&
+        developer["Github Id"] == %x[git config --global github.user].chomp
+
+    if check
+      puts "Switch successfull."
+    else
+      puts "Problem switching to #{developer['Full Name']}."
+    end
+  end
+
+  def self.switch_identity(developer)
+    identity = File.join("~/.ssh/", developer['SSH Identity'])
+    print "Switching ssh identity to #{File.expand_path(identity)}, "
+    $stdout.flush
+
+    if File.exists?(File.expand_path(identity))
+      system("ssh-add -D")
+      system("ssh-add #{identity}")
+      check_identity(developer)
+    else
+      puts "#{developer["Full Name"]}'s ssh keys not found. Skipping identity switch."
+    end
+  end
+
+  def self.check_identity(developer)
+    identity = File.join("~/.ssh/", developer['SSH Identity'])
+    identities = %x[ssh-add -l].split("\n")
+
+    print "Checking identity, "
+
+    $stdout.flush
+
+    if identities.length > 1
+      puts "Problem releasing existing identities. Possible amibiguity."
+    else
+      puts "Successfully switched ssh identity to #{File.expand_path(identity)}"
     end
   end
 end
