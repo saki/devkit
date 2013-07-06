ab6581968e50baeb70fe3b94f3d5bbb5d91533a6
Tripura Konda <tripura@eggheadcreative>
2013-07-06 13:13:53 +0530

    Renaming switch method to choose

--- lib/devkit.rb
+++ lib/devkit.rb
@@ -7,4 +7,11 @@ require 'devkit/ssh_identity'
 require 'devkit/version'
 
 module Devkit
+  EXPIRY_TIME = 14400
+
+  class << self
+    def bin_path
+      %x[which devkit].chomp
+    end
+  end
 end
--- lib/devkit/ssh_identity.rb
+++ lib/devkit/ssh_identity.rb
@@ -1,22 +1,19 @@
 module Devkit
   module SshIdentity
     class << self
-      def switch(developer)
+      def choose(developer)
         identity = File.join("~/.ssh/", developer['SSH Identity'])
         print "Switching ssh identity to #{File.expand_path(identity)}, "
         $stdout.flush
 
         if File.exists?(File.expand_path(identity))
           system("ssh-add -D")
+          system("ssh-add #{identity}")
         else
           puts "#{developer["Full Name"]}'s ssh keys not found. Skipping identity switch.".red
         end
 
         check(developer)
-      end
-
-      def expire_command(time)
-        %Q(sleep #{to_seconds(time)} && #{Devkit.bin_path} --drop&)
       end
 
       def check(developer)
